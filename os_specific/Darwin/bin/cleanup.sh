b6021348ff53e9fd9cc4fadb3758bf5dbc38ee8b
Andreas Wachowski <andreas.wachowski@gmail.com>
2015-11-05 21:36:47 +0100

    feat(cron): add cleanup

---
+++ os_specific/Darwin/bin/cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# vi: tw=0
+/usr/bin/find $HOME/{Downloads,books,mendeley_inbox,tmp,.tmux/resurrect} -mount -ctime +30 -mindepth 1 # -delete
+/usr/bin/find $HOME/Desktop -name 'Screen Shot *' -mount -ctime +60 -mindepth 1 # -delete
