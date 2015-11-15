bb4965b1a1081e102bc43c7f48494842de31e6c4
Andreas Wachowski <andreas.wachowski@gmail.com>
2015-11-15 09:21:39 +0100

    fix(vim): avoid find warning

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -3,4 +3,5 @@
 
 # Cleanup vim swap files
 VIMSWAPDIR=$HOME/tmp/vim
-find $VIMSWAPDIR -type d -empty -delete
+# 2>/dev/null: ignore warning on relative dirs potentially unsafe
+find $VIMSWAPDIR -type d -empty -delete 2>/dev/null
