029404e215f3f91083ec537cf0259c4fd1b29b2b
Andreas Wachowski <andreas.wachowski@gmail.com>
2015-11-15 09:29:03 +0100

    feat(cleanup): handle tmp-vim separately

--- os_specific/Darwin/bin/cleanup.sh
+++ os_specific/Darwin/bin/cleanup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # vi: tw=0
-/usr/bin/find $HOME/{Downloads,books,mendeley_inbox,tmp,.tmux/resurrect} -mount -ctime +30 -mindepth 1 -delete
-/usr/bin/find $HOME/Desktop -name 'Screen Shot *' -mount -ctime +60 -mindepth 1 -delete
+
+# vim-swap files in ~/tmp/vim are exculded from the cleanup, because theoretically,
+# vim swapfiles that are still in use would be deleted if that vim-session were
+# running longer than 30 days.
+/usr/bin/find $HOME/{Downloads,books,mendeley_inbox,tmp,.tmux/resurrect} -mount \
+  -ctime +30 -mindepth 1 ! -name '$HOME/tmp/vim/*' -delete
+/usr/bin/find $HOME/Desktop -name 'Screen Shot *' -mount \
+  -ctime +60 -mindepth 1 -delete
