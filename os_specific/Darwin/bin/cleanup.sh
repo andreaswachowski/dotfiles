ac17f4082bee506fcc41060e9be7b81ead44ca2b
Andreas Wachowski <andreas.wachowski@gmail.com>
2016-09-22 18:20:55 +0200

    feat(cleanup): add .Trash to monthly cleanup job

--- os_specific/Darwin/bin/cleanup.sh
+++ os_specific/Darwin/bin/cleanup.sh
@@ -4,7 +4,7 @@
 # vim-swap files in ~/tmp/vim are exculded from the cleanup, because theoretically,
 # vim swapfiles that are still in use would be deleted if that vim-session were
 # running longer than 30 days.
-/usr/bin/find $HOME/{Downloads,books,mendeley_inbox,tmp,.tmux/resurrect} -mount \
+/usr/bin/find $HOME/{.Trash,Downloads,books,mendeley_inbox,tmp,.tmux/resurrect} -mount \
   -ctime +30 -mindepth 1 ! -name '$HOME/tmp/vim/*' -delete
 /usr/bin/find $HOME/Desktop -name 'Screen Shot *' -mount \
   -ctime +60 -mindepth 1 -delete
