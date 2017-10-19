e641708e6a6d76388649d6c974db4187a98d4ae3
Andreas Wachowski <andreas.wachowski@wlw.de>
2017-10-19 16:07:50 +0200

    feat(terminal): Disable scrollbar in full screen mode

---
+++ os_specific/Darwin/setups/terminal.sh
@@ -0,0 +1,2 @@
+# https://apple.stackexchange.com/questions/20605/how-do-i-make-terminal-apps-scrollbars-disappear-in-lion
+defaults write com.apple.Terminal AppleShowScrollBars -string WhenScrolling
