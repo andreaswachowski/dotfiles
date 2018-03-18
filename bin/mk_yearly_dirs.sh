0a508f04da4d542c2c18720842a07770af7cc9ef
Andreas Wachowski <andreas.wachowski@gmail.com>
2018-03-18 19:42:17 +0100

    feat(bin): mk_yearly_dirs.sh: move notes dir to Dropbox

--- bin/mk_yearly_dirs.sh
+++ bin/mk_yearly_dirs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# mkdir ownCloud/<Year>, inside:
 for month in 01_january 02_february 03_march 04_march 05_may 06_june 07_july 08_august 09_september 10_october 11_november 12_december
 do
   mkdir $month
@@ -7,4 +8,5 @@ done
 mkdir -p doc/magazines/{acm_computing_surveys,cacm,ieee_computer,ieee_software,mit_technology_review}
 mkdir -p finanzen/{t-mobile,targobank,telekom,vattenfall,vtb,steuererklärung,flatex,diba,quicken2017,anlage}
 mkdir -p fortbildung/{lectures,books,exercises}
-mkdir notes
+
+echo "Don't forget to mkdir Dropbox/notes/<Year>"
