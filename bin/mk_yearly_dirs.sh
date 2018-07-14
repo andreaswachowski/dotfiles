0449a16c8025c572219a71b7efce2d7f214ddb43
Andreas Wachowski <andreas.wachowski@gmail.com>
2018-07-14 16:05:59 +0200

    refactor: shellcheck mk_yearly_dirs

--- bin/mk_yearly_dirs.sh
+++ bin/mk_yearly_dirs.sh
@@ -3,15 +3,15 @@
 set -e
 
 if [ $# -ne 1 ]; then
-  echo "Usage: $(basename $0) <year>"
+  echo Usage: "$(basename "$0")" "<year>"
   exit 1
 fi
 
 CURRENT_DIR=$(pwd)
 YEAR=$1
 
-mkdir ~/Documents/ownCloud/$YEAR
-cd ~/Documents/ownCloud/$YEAR
+mkdir "$HOME/Documents/ownCloud/$YEAR"
+cd "$HOME/Documents/ownCloud/$YEAR"
 for month in 01_january 02_february 03_march 04_march 05_may 06_june 07_july 08_august 09_september 10_october 11_november 12_december
 do
   mkdir $month
@@ -21,11 +21,11 @@ mkdir -p finanzen/{t-mobile,targobank,telekom,vattenfall,vtb,steuererklärung,fl
 mkdir -p fortbildung/{lectures,books,exercises,anki}
 
 NOTES=~/Documents/Dropbox/notes/$YEAR
-mkdir $NOTES
-cd $NOTES
+mkdir "$NOTES"
+cd "$NOTES"
 mkdir -p reading/articles
 mkdir -p reading/books
 mkdir todos
 
-cd $CURRENT_DIR
+cd "$CURRENT_DIR"
 echo "Don't forget to archive old directories"
