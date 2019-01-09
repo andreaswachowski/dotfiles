f983e9d1a134fdb0944498e3576e1f06912a0a6e
Andreas Wachowski <andreas.wachowski@gmail.com>
2019-01-09 19:07:16 +0100

    feat(setup): OS/Distro/Window Manager detection
    Flesh out more generalized setup, distinguishing by OS, distro,
    package installer, and window manager.
    
    * Add some GNOME configuration settings
      (capslock/escape setting, terminal keyboard shortcuts)

--- setup.sh
+++ setup.sh
@@ -13,6 +13,151 @@ Options:
 HERE
 }
 
+abort() {
+  >&2 echo "$1. Aborting."
+  exit 1
+}
+
+execute() {
+	DOTFILES=$DOTFILES $1
+}
+
+execute_if_available() {
+  [ -x "$1" ] && $1
+}
+
+log() {
+  echo "*** $*"
+}
+
+is_available() {
+  command -v "$1" >/dev/null 
+}
+
+detect_os() {
+  log "Detecting OS ..."
+  case $(uname) in
+    Linux)
+      OS=linux
+      ;;
+    Darwin)
+      OS=Darwin
+      ;;
+    *)
+      abort "Unknown OS $(uname)"
+  esac
+  log "OS=$OS"
+}
+
+abort_detect_distributor_id() {
+  abort "Cannot determine distributor ID ($1)"
+}
+
+detect_distributor_id() {
+  log Detecting distribution ...
+  case $(uname) in
+    Linux)
+      if is_available lsb_release; then
+        DISTRIBUTOR_ID=$(lsb_release --all | grep 'Distributor ID' | sed 's/.*:\t//')
+      else
+        OS_ID_AS_PER_ETC_RELEASE=$(grep '^ID' /etc/*-release | cut -d= -f2)
+        case $OS_ID_AS_PER_ETC_RELEASE in
+          qts)
+            DISTRIBUTOR_ID=QNAP
+            ;;
+          *)
+            abort_detect_distributor_id "no suitable ID found in /etc/*-release"
+        esac
+      fi
+      ;;
+    Darwin)
+      DISTRIBUTOR_ID=Apple
+      ;;
+    *)
+      abort_detect_distributor_id "unknown OS \"$(uname)\""
+  esac
+  log "DISTRIBUTOR_ID=$DISTRIBUTOR_ID"
+}
+
+determine_package_installer() {
+  log "Determining package installer ..."
+  PACKAGE_INSTALLER_DEFINITION_FILE="$SETUP_HELPER_DIR/by_distributor_id/$DISTRIBUTOR_ID/package_manager"
+  if [ -f "$PACKAGE_INSTALLER_DEFINITION_FILE" ]; then
+    PACKAGE_INSTALLER=$(cat "$PACKAGE_INSTALLER_DEFINITION_FILE")
+  else
+    abort "Don't know package installer for distros by \"$DISTRIBUTOR_ID\" (cannot find \"$PACKAGE_INSTALLER_DEFINITION_FILE\")"
+  fi
+  log "PACKAGE_INSTALLER=$PACKAGE_INSTALLER"
+}
+
+ensure_package_installer_availability() {
+  PACKAGE_INSTALLER_FULL_PATH=$(command -v "$PACKAGE_INSTALLER")
+  is_available "$PACKAGE_INSTALLER" || "$SETUP_HELPER_DIR/by_package_manager/$PACKAGE_INSTALLER/install_self"
+  PACKAGE_INSTALLER_FULL_PATH=$(command -v "$PACKAGE_INSTALLER")
+  [ -n "$PACKAGE_INSTALLER_FULL_PATH" ] || abort "Failed to initialize $PACKAGE_INSTALLER"
+}
+
+prepare_package_installer() {
+  ensure_package_installer_availability
+  execute_if_available "$SETUP_HELPER_DIR/by_package_manager/$PACKAGE_INSTALLER/update_packages"
+}
+
+configure_install_cmd() {
+  log "Configuring install command ..."
+  INSTALL_CMD_FILE=$SETUP_HELPER_DIR/by_package_manager/$PACKAGE_INSTALLER/install_cmd
+  if [ -f "$INSTALL_CMD_FILE" ]; then
+    INSTALL_CMD=$(cat "$SETUP_HELPER_DIR/by_package_manager/$PACKAGE_INSTALLER/install_cmd")
+  else
+    abort "Cannot configure package-install command (cannot find \"$INSTALL_CMD_FILE\")"
+  fi
+  log "INSTALL_CMD=$INSTALL_CMD"
+}
+
+detect_window_manager() {
+  log "Detecting window manager ..."
+  if [ $OS = "Darwin" ]; then
+    WINDOW_MANAGER="Mac OS";
+  else
+    is_available wmctrl || $INSTALL_CMD wmctrl # fine if installation fails
+    is_available wmctrl && WINDOW_MANAGER=$(wmctrl -m | grep ^Name | sed 's/Name: //')
+    # still not assigned? wmctrl not installable, possibly a server system
+    # like QTS
+    [ -z "$WINDOW_MANAGER" ] && WINDOW_MANAGER="N/A"
+  fi;
+  log "WINDOW_MANAGER=$WINDOW_MANAGER"
+}
+
+window_manager_to_directory() {
+	case "$1" in
+		KWin) echo kde
+			;;
+		GNOME\ Shell) echo gnome
+			;;
+		Mac\ OS) echo mac_os
+			;;
+		N/A) echo none
+			;;
+		*) echo unknown
+	esac
+}
+
+setup_basics() {
+  detect_os
+  detect_distributor_id
+  determine_package_installer
+  prepare_package_installer
+  configure_install_cmd
+  detect_window_manager
+}
+
+install_fundamentals() {
+  log "Installing fundamental packages ..."
+  case $PACKAGE_INSTALLER in
+    brew) ;;
+    *) $INSTALL_CMD vim git tmux fzf wmctrl
+  esac
+}
+
 # --- process options (example)
 while getopts ":hv" OPT
 do
@@ -37,12 +182,27 @@ done
 
 shift $(expr $OPTIND - 1)
 
-# --- process the arguments
 DOTFILES=~/dotfiles # Assume dotfiles is checked out here
-if [ ! -d "$DOTFILES" ]; then
-  echo "Dotfiles not found at $DOTFILES. Set this variable correctly, then continue. Aborting."
-  exit 1
+
+[ -d "$DOTFILES" ] || \
+  abort "Dotfiles not found at $DOTFILES. Set this variable correctly, then continue"
+
+SETUP_HELPER_DIR=$DOTFILES/setup_helpers
+
+setup_basics
+
+WINDOW_MANAGER_DIR=$SETUP_HELPER_DIR/by_window_manager/$(window_manager_to_directory "$WINDOW_MANAGER")
+if [ -d "$WINDOW_MANAGER_DIR" ]; then
+  log "Executing window-manager specific scripts from $WINDOW_MANAGER_DIR ..."
+  for script in "$WINDOW_MANAGER_DIR"/*
+  do
+    execute "$WINDOW_MANAGER_DIR/$script"
+  done
+else
+  log "No window-manager specific scripts found (looked in $WINDOW_MANAGER_DIR) ..."
 fi
+
+exit 1
 
 gitfallback() {
   echo "git not found. skipping git $*"
