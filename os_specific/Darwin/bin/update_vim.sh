3eb702fb1668844c4e13b6d5a7343027f9fd508d
Andreas Wachowski <andreas.wachowski@gmail.com>
2019-08-25 10:19:33 +0200

    feat(vim): check-in vim update scripts

---
+++ os_specific/Darwin/bin/update_vim.sh
@@ -0,0 +1,36 @@
+set -e
+
+finish() {
+  cd - >/dev/null
+}
+
+trap finish EXIT
+
+cd ~/local/src/vim
+
+compile() {
+  git merge
+  configure_vim.sh
+  make -j 4
+  echo "vim should dynamically link to the system-provided Ruby lib, or ruby will segfault"
+  echo "If desired, test with ':ruby 1' inside vim"
+  echo "Checking with otool -L src/vim | grep Rub; ls -l src/vim"
+  echo
+  otool -L src/vim | grep Rub; ls -l src/vim
+}
+
+if git fetch; then
+  git log --oneline --graph --all --decorate master..origin/master
+  # http://alvinalexander.com/linux-unix/shell-script-how-prompt-read-user-input-bash
+  printf "Continue (Y/n)? "
+  read -r answer
+  answer=${answer:-Y}
+  case $answer in
+    [yY]* ) compile;;
+    [nN]* ) exit;;
+    *) ;;
+  esac
+else
+  echo git fetch failed. Fix this and try again.
+  exit 1
+fi
