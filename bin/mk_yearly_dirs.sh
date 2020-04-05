5f228f3b33426c153cf65f97f85d060519698d12
Andreas Wachowski <andreas.wachowski@gmail.com>
2020-04-05 06:39:23 +0200

    fix(mk_yearly_dirs): rename 04_march to 04_april

--- bin/mk_yearly_dirs.sh
+++ bin/mk_yearly_dirs.sh
@@ -12,7 +12,7 @@ YEAR=$1
 
 mkdir "$HOME/Documents/ownCloud/$YEAR"
 cd "$HOME/Documents/ownCloud/$YEAR"
-for month in 01_january 02_february 03_march 04_march 05_may 06_june 07_july 08_august 09_september 10_october 11_november 12_december
+for month in 01_january 02_february 03_march 04_april 05_may 06_june 07_july 08_august 09_september 10_october 11_november 12_december
 do
   mkdir $month
 done
