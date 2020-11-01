f95e09089c9927ae37021ac3c3de25774fc97dd7
Andreas Wachowski <andreas.wachowski@gmail.com>
2020-11-01 09:34:10 +0100

    feat(rsync_audio): Add rsync_audio script

---
+++ bin/rsync_audio.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+
+# Sync from salt to cumin
+# vi: tw=0 ts=2 sw=2 expandtab
+# TODO:
+# Option -n passed on commandline of this script
+# Option -d [cumin/imac] passed on commandline of this script
+#
+# Whenever I move a directory, the original directory is left
+# dangling after the rsync on cumin, because, on nutmeg, it includes also
+# the '*__thumb*' files not available on salt.
+# Using delete-excluded will delete *all* the __thumb-files, whether or not
+# they are in a moved directory.
+#
+# As usual, others are having exactly the same problem:
+# (google for "rsyn cannot delete non-empty directory")
+#
+# http://www.linuxquestions.org/questions/slackware-14/cant-remove-non-empty-directory-with-rsync-694546/
+#
+# The only solution I currently have is to manually delete those files
+# I could extend this script to analyze the error messages and then do it
+# automatically
+
+displayUsageScreen() {
+	# <<- as opposed to << strips the leading tabs from the output
+	cat <<- HERE
+	Usage: $ScriptName [-hmn] [-d host] [Subdirectory]
+
+  Synchronizes audio files between hosts.
+  Unless a subdirectory is given the whole /audio directory is used.
+
+	Options:
+	           -h : Displays this screen
+	           -m : Performs a backup of the Amarok mysql database first
+	           -n : Appends "n" to rsync to perform a dry run
+	     -d host: Specifies the destination host. Default is cumin
+	HERE
+}
+
+ScriptName=$(basename "$0")
+
+# -------------------------------------------------------------------------
+while getopts "hmnd:" OPT
+do
+    case "$OPT" in
+    d) DEST=$OPTARG
+       ;;
+
+    m) mysqldump -u amarokuser -pbla32mar0K amarokdb | gzip > /audio/amarok.sql.gz
+       ;;
+
+    n) RSYNCOPTS=n
+       ;;
+
+    h) displayUsageScreen
+       exit 1
+       ;;
+
+    :) print -u2 "Incorrect Syntax: -${OPTARG} needs argument."
+       displayUsageScreen
+       exit 1
+       ;;
+
+    \?) print -u2 "Incorrect Syntax: -${OPTARG} bad option."
+        displayUsageScreen
+        exit 1
+        ;;
+    esac
+done
+
+shift $(("$OPTIND" - 1))
+
+
+case "$(hostname)" in
+  salt) SRC=/audio/audiolibrary
+    ;;
+  anwa-macbook) SRC=/Volumes/audio/audiolibrary
+    ;;
+  *) echo "Unknown source. Aborting"
+    exit 1
+    ;;
+esac
+
+if [ -z "$DEST" ]; then
+  DEST=cumin
+fi
+
+if [ -n "$1" ]; then
+  DIR=$1
+fi
+
+case "$DEST" in
+  cumin) ROOT=/share/audio/audiolibrary
+    ;;
+  imac) ROOT=/Volumes/HDD2/audio/audiolibrary
+    ICONV="--iconv=UTF8,UTF8-MAC"
+    RSYNC_PATH="--rsync-path=/usr/local/bin/rsync"
+    ;;
+  anwa-macbook) ROOT=/Volumes/audio/audiolibrary
+    ICONV="--iconv=UTF8,UTF8-MAC"
+    RSYNC_PATH="--rsync-path=/usr/local/bin/rsync"
+    ;;
+  *) echo "Unknown destination $DEST, aborting."
+    exit 1
+    ;;
+esac
+
+#rsync -av --exclude '*__thumb*' --delete-after /audio/ cumin:/share/audio
+#rsync -av --iconv=UTF8,UTF8-MAC --exclude '*__thumb*' --delete-after /audio/ imac:/Volumes/HDD2/audio
+
+echo rsync -av${RSYNCOPTS} ${ICONV} ${RSYNC_PATH} --exclude '*__thumb*' --exclude 'lost+found' --delete-after $SRC${DIR:+/$DIR}/ ${DEST}:${ROOT}${DIR:+/$DIR}
+rsync -av${RSYNCOPTS} ${ICONV} ${RSYNC_PATH} --exclude '.DS_Store' --exclude '*__thumb*' --exclude 'lost+found' --delete-after $SRC${DIR:+/$DIR}/ ${DEST}:${ROOT}${DIR:+/$DIR}
