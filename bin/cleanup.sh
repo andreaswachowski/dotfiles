11d884c6576537d732afb04ffa355082169e473e
Andreas Wachowski <andreas.wachowski@gmail.com>
2020-12-18 19:29:24 +0100

    fix(vim): make cleanup portable

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 # vi: tw=0
 
+PLATFORM=$(uname)
+
+case $PLATFORM in
+    Linux)
+        FIND_REGEX="-regextype posix-extended"
+        ;;
+    Darwin)
+        FIND_REGEX="-E"
+        ;;
+    *)
+        echo Unknown platform, aborting
+        exit 1
+        ;;
+esac
+
 # Cleanup vim swap files
 VIMSWAPDIR=$HOME/tmp/vim
 # see http://stackoverflow.com/questions/762348/how-can-i-exclude-all-permission-denied-messages-from-find
 # for a discussion on the error output redirection
-VIM_PIDS=$(ps -c | grep vim | cut -f1 -d' ' | paste -sd "|" -)
 
-find -E "$VIMSWAPDIR" -type d ! -regex ".*($VIM_PIDS|$VIMSWAPDIR)" -delete 2> >(grep -v 'relative path potentially not safe' >&2)
+REGEX="$(echo $(pgrep vim) "$VIMSWAPDIR" | tr ' ' '|')"
+
+find "$VIMSWAPDIR" -type d $FIND_REGEX ! -regex ".*($REGEX)" -delete 2> >(grep -v 'relative path potentially not safe' >&2)
