dbf9a17070b8d5f902a8eeceeed335508c249a2c
Andreas Wachowski <andreas.wachowski@gmail.com>
2020-12-18 19:35:51 +0100

    fix(vim): "-E" must come BEFORE path in macos find

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # vi: tw=0
 
-PLATFORM=$(uname)
 
-case $PLATFORM in
+VIMSWAPDIR=$HOME/tmp/vim
+REGEX="$(echo $(pgrep vim) "$VIMSWAPDIR" | tr ' ' '|')"
+
+case $(uname) in
     Linux)
-        FIND_REGEX="-regextype posix-extended"
+        FIND_OPTS="$VIMSWAPDIR -regextype posix-extended"
         ;;
     Darwin)
-        FIND_REGEX="-E"
+        FIND_OPTS="-E $VIMSWAPDIR"
         ;;
     *)
         echo Unknown platform, aborting
@@ -17,10 +19,8 @@ case $PLATFORM in
 esac
 
 # Cleanup vim swap files
-VIMSWAPDIR=$HOME/tmp/vim
 # see http://stackoverflow.com/questions/762348/how-can-i-exclude-all-permission-denied-messages-from-find
 # for a discussion on the error output redirection
 
-REGEX="$(echo $(pgrep vim) "$VIMSWAPDIR" | tr ' ' '|')"
 
-find "$VIMSWAPDIR" -type d $FIND_REGEX ! -regex ".*($REGEX)" -delete 2> >(grep -v 'relative path potentially not safe' >&2)
+find $FIND_OPTS -type d ! -regex ".*($REGEX)" -delete 2> >(grep -v 'relative path potentially not safe' >&2)
