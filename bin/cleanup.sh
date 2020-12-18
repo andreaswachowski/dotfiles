a5bcdb8961596e8506b0a656194a26d6e7531c38
Andreas Wachowski <andreas.wachowski@gmail.com>
2020-12-18 09:21:22 +0100

    fix(vim): avoid deleting in-use vim backup dirs
    The backup directory only briefly contains a file while writing
    a new version. Hence it is empty most of the time. We must ensure
    that running vim sessions keep their backup directory, or else
    errors ("Cannot write backup!") pop up when attempting to write.

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -5,4 +5,6 @@
 VIMSWAPDIR=$HOME/tmp/vim
 # see http://stackoverflow.com/questions/762348/how-can-i-exclude-all-permission-denied-messages-from-find
 # for a discussion on the error output redirection
-find "$VIMSWAPDIR" -type d -empty -delete 2> >(grep -v 'relative path potentially not safe' >&2)
+VIM_PIDS=$(ps -c | grep vim | cut -f1 -d' ' | paste -sd "|" -)
+
+find -E "$VIMSWAPDIR" -type d ! -regex ".*($VIM_PIDS|$VIMSWAPDIR)" -delete 2> >(grep -v 'relative path potentially not safe' >&2)
