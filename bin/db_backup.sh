96b544acd28cd3ef9daabb06e91e4effa9bcda24
Andreas Wachowski <andreas.wachowski@gmail.com>
2021-03-11 23:04:26 +0100

    feat(bin): Make db_backup robust towards empty backups

--- bin/db_backup.sh
+++ bin/db_backup.sh
@@ -10,25 +10,66 @@ if [[ -z "$1" ]]; then
     exit 1
 fi
 
+DATE=/opt/bin/date
+FIND=/opt/bin/find
+GZIP=/opt/bin/gzip
+SED=/opt/bin/sed
+
 DB_BACKUP_SCRIPT="$1"; shift
 DB="$1"; shift
 DIR="/share/mysql_backup/cumin/$DB"
-
-GZIP=/opt/bin/gzip
 
 KEEP_DAILY=30
 KEEP_WEEKLY=5
 KEEP_MONTHLY=12
 
+function date_greater_or_equal {
+  # compare seconds since epoch for the dates
+  s1=$($DATE -d "$1" +%s)
+  s2=$($DATE -d "$2" +%s)
+
+  if [ $s1 -ge $s2 ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
 function rotate {
     rotation=$1
-    fdate=$(date +%Y-%m-%d -d "$date")
-    file=$DIR/daily/*$fdate*.gz # In Assignment, globs are *not* expanded
-    # In the if-statement, $file must be unquoted to be expanded
-    if [ -f $file ]; then
-        cp -p $file "$DIR/$rotation/" || abort
-    else
-        echo
+    # The rotation function shall be robust against missing backups.
+    # For example, if the weekly rotation run's on Sunday, and only backups
+    # until Thursday exist, then we shall rotate Thursday's (ie the most
+    # recent) backup as this week's backup.
+    case $rotation in
+      weekly)
+        # monday of current week (because rotation on Sunday)
+        earliest=$($DATE -dlast-monday +%Y-%m-%d)
+        most_recent_backup=$(ls -t $DIR/daily/${DB}_*.sql.gz 2>/dev/null | head -1)
+        ;;
+      monthly)
+        # first day of previous month
+        earliest=$($DATE --date="$(date +'%Y-%m-01') - 1 month" +%Y-%m-01)
+        previous_month=$($DATE --date="$(date +'%Y-%m-01') - 1 month" +%m)
+        most_recent_backup=$(ls -t $DIR/daily/${DB}_*-${previous_month}-*.sql.gz 2>/dev/null | head -1)
+        ;;
+      yearly)
+        # first day of previous year
+        earliest=$($DATE --date="$(date +'%Y-%m-01') - 1 year" +%Y-01-01)
+        previous_year=$($DATE --date="$(date +'%Y-%m-01') - 1 year" +%Y)
+        most_recent_backup=$(ls -t $DIR/daily/${DB}_${previous_year}-*.sql.gz  2>/dev/null | head -1)
+        ;;
+    esac
+
+    if [ -f "$most_recent_backup" ]; then
+      date_of_most_recent_backup=$(basename $most_recent_backup .sql.gz | sed "s/${DB}_//" -)
+
+      date_greater_or_equal "$date_of_most_recent_backup" "$earliest" 
+      if [ $? -eq 0 ]; then
+          cp -p $most_recent_backup "$DIR/$rotation/" || abort
+      fi
+    # else
+    #  echo No backup found for $rotation rotation from $earliest
     fi
 }
 
@@ -48,14 +89,17 @@ mkdir -p "$DIR/weekly" || abort
 mkdir -p "$DIR/monthly" || abort
 mkdir -p "$DIR/yearly" || abort
 
-date=$(date +%Y-%m-%d) || abort
-day=$(date -d "$date" +%d) || abort
-weekday=$(date -d "$date" +%w) || abort
-month=$(date -d "$date" +%m) || abort
+date=$($DATE +%Y-%m-%d) || abort
+day=$($DATE -d "$date" +%d) || abort
+weekday=$($DATE -d "$date" +%w) || abort
+month=$($DATE -d "$date" +%m) || abort
 
 # Do the daily backup
-$DB_BACKUP_SCRIPT | $GZIP > "$DIR/daily/${DB}_$date.sql.gz"
-test ${PIPESTATUS[0]} -eq 0 || abort
+SQL_DUMP=/tmp/${DB}_$date.sql
+$DB_BACKUP_SCRIPT > "$SQL_DUMP"
+if [ -s "$SQL_DUMP" ]; then
+  $GZIP "$SQL_DUMP" && mv $SQL_DUMP.gz "$DIR/daily/${DB}_$date.sql.gz"
+fi
 
 # Perform rotations
 if [[ "$weekday" == "0" ]]; then
