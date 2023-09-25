9700c46dd750a08815a93168aca9e7338acb24c8
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-09-25 18:03:24 +0200

    feat(nvim): show tab-local directories in tabline

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -162,4 +162,11 @@ vim.keymap.set('n', '<c-h>', '<c-w>h')
 vim.keymap.set('n', '<c-k>', '<c-w>k')
 vim.keymap.set('n', '<c-j>', '<c-w>j')
 
-vim.cmd([[source ~/.vimrc]])
+-- [[ Import other stuff ]]
+
+-- Show directories in tabline.
+-- The idea is to handle one project per tab, using the buffer list for all
+-- project-related files. Thus, each tab is set to a different tab-local
+-- directory.
+vim.cmd([[source ~/.vim/tabline.vim]])
+-- vim.cmd([[source ~/.vimrc]])
