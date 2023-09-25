d5608c3244d538d93543339034fcef7cf3281386
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-09-25 12:39:24 +0200

    feat(nvim): show relative path in status line

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -106,11 +106,19 @@ require("lazy").setup({
       },
       sections = {
         lualine_a = {'branch', 'diff', 'diagnostics'},
-        lualine_b = {'filename'},
+        lualine_b = {{'filename', path = 1}},
         lualine_c = {},
         lualine_x = {'encoding', 'fileformat', 'filetype'},
         lualine_y = {'progress'},
         lualine_z = {'location'}
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {{'filename', path = 1}},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
       },
     },
   },
