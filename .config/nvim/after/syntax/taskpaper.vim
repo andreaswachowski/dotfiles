0075515e0ff969a57b42dd842d4b1b7913e5f440
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-09-26 12:34:16 +0200

    feat(nvim): add plugin taskpaper.vim

---
+++ .config/nvim/after/syntax/taskpaper.vim
@@ -0,0 +1,1 @@
+/Users/andreas/.vim/after/syntax/taskpaper.vim
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -209,6 +209,21 @@ require("lazy").setup({
       vim.fn["mkdp#util#install"]()
     end,
   },
+
+  {
+    'andreaswachowski/taskpaper.vim',
+    lazy = false,
+    keys = {
+       ---@diagnostic disable-next-line: missing-fields
+      {
+        "<leader>tc", "<cmd>call taskpaper#toggle_tag('created', taskpaper#date())<cr>",
+        desc = "Toggle created tag with current date"
+      },
+    },
+    config = function()
+      vim.cmd([[autocmd FileType taskpaper setlocal shiftwidth=2 tabstop=2]])
+    end
+  }
 }, {})
 
 -- [[ Setting options ]]
