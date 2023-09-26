b094cc55047b003225d6d17c3d647b98bf9c1ecf
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-09-26 11:47:09 +0200

    feat(nvim): add plugin nvim-surround

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -45,6 +45,15 @@ require("lazy").setup({
 
   { 'tpope/vim-obsession' },
 
+  {
+    'kylechui/nvim-surround',
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup()
+    end
+  },
+
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
