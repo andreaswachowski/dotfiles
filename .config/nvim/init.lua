9065bbe174a528fbb7e1d9e2e936b051553359f0
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-09-26 16:50:59 +0200

    fix(nvim): use noremap and silent for window navigation
    None (or hardly any) of the telescope keymappings seemed to work. In particular,
    C-/ did not display the help.
    
    I am not entirely sure if this fixed it, but it doesn't hurt to define the
    mappings like this.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -277,10 +277,10 @@ vim.o.winminheight = 0 -- minimize a window to just its status bar
 
 -- Vim Tip 173: Quick movement between split windows
 -- https://vim.fandom.com/wiki/Switch_between_Vim_window_splits_easily
-vim.keymap.set('n', '<c-l>', '<c-w>l')
-vim.keymap.set('n', '<c-h>', '<c-w>h')
-vim.keymap.set('n', '<c-k>', '<c-w>k')
-vim.keymap.set('n', '<c-j>', '<c-w>j')
+vim.keymap.set('n', '<c-l>', '<c-w>l', { noremap = true, silent = true })
+vim.keymap.set('n', '<c-h>', '<c-w>h', { noremap = true, silent = true })
+vim.keymap.set('n', '<c-k>', '<c-w>k', { noremap = true, silent = true })
+vim.keymap.set('n', '<c-j>', '<c-w>j', { noremap = true, silent = true })
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
