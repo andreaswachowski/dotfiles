afc5ae92e7bb56d18143e1982a6725832e714601
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-09-26 12:09:53 +0200

    feat(nvim): disable missing fields warnings
    From [1]:
    
    > It looks like these fields were not marked as optional when the function was
    > defined. Until they are fixed, you can disable this diagnostic using
    > diagnostics.disable.
    
    [1] https://github.com/LuaLS/lua-language-server/discussions/2271
    [2] https://github.com/folke/lazy.nvim/discussions/1027
    [3] https://github.com/nvim-treesitter/nvim-treesitter/issues/5297

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -61,11 +61,13 @@ require("lazy").setup({
   {
     'tpope/vim-fugitive',
     keys = {
+      ---@diagnostic disable: missing-fields
       { "<Bslash>cs", "<cmd>Git<cr>", desc = "Git status" },
       { "<Bslash>cc", "<cmd>Git commit<cr>", desc = "Git commit" },
       { "<Bslash>cv", "<cmd>Gdiff<cr>", desc = "Git diff" },
       { "<Bslash>cl", "<cmd>Gclog<cr>", desc = "Git log" },
       { "<Bslash>cb", "<cmd>Git blame<cr>", desc = "Git blame" },
+      ---@diagnostic enable: missing-fields
     },
     config = function()
       -- Include (mailmap'ed) author and a date indication in the fugitive
@@ -381,6 +383,7 @@ local luasnip = require 'luasnip'
 require('luasnip.loaders.from_vscode').lazy_load()
 luasnip.config.setup {}
 
+---@diagnostic disable-next-line: missing-fields
 cmp.setup {
   snippet = {
     expand = function(args)
