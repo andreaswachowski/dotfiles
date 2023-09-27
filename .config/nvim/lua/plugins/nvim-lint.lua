855de940f84c1b2d9d0c7c3f2786d7b8deb3e8a8
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-09-27 18:22:08 +0200

    feat(nvim): lint with rubocop while editing

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -3,15 +3,15 @@ return {
   config = function()
     local rubocop = require('lint').linters.rubocop
     rubocop.cmd = 'bundle'
-    rubocop.args = { 'exec', 'rubocop', '--format', 'json', '--force-exclusion' }
-    -- rubocop.stdin = true
+    rubocop.args = { 'exec', 'rubocop', '--format', 'json', '--force-exclusion', '--stdin', vim.api.nvim_buf_get_name(0) }
+    rubocop.stdin = true
     require('lint').linters_by_ft = {
       ruby = {
         "rubocop"
       }
     }
     vim.api.nvim_create_autocmd(
-      { "BufWritePost" },
+      { "BufReadPost", "BufWritePost", "InsertLeave", "TextChanged" },
       { callback = function() require("lint").try_lint() end, }
     )
   end
