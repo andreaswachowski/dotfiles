dcfa7c35f844465bf7858c337233364721f467b9
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-09-28 14:22:36 +0200

    refactor(nvim): extract keymaps from init.lua to separate file

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,14 @@
+-- Vim Tip 173: Quick movement between split windows
+-- https://vim.fandom.com/wiki/Switch_between_Vim_window_splits_easily
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set('n', '<c-l>', '<c-w>l', opts)
+vim.keymap.set('n', '<c-h>', '<c-w>h', opts)
+vim.keymap.set('n', '<c-k>', '<c-w>k', opts)
+vim.keymap.set('n', '<c-j>', '<c-w>j', opts)
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
