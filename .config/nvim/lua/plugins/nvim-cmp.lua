46f85e106759d14937a4751033e44eb13c3d1060
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-09-28 11:02:21 +0200

    refactor(nvim): correct nvim-cmp deps
    In [^1], dependencies are specified as:
    
        A list of plugin names or plugin specs that should be loaded when the plugin loads.
    
    In [^2], it says:
    
        {
          "L3MON4D3/LuaSnip",
          dependencies = { "rafamadriz/friendly-snippets" },
        }
    
    Note: LuaSnip does NOT depend on friendly-snippets. It is the other way
    around; the wording is a bit confusing.
    
    [^1]: https://github.com/folke/lazy.nvim#-plugin-spec
    [^2]: https://github.com/rafamadriz/friendly-snippets#with-lazynvim

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,15 +1,17 @@
 return {
   -- Autocompletion
   'hrsh7th/nvim-cmp',
+
   dependencies = {
-    -- Snippet Engine & its associated nvim-cmp source
-    'L3MON4D3/LuaSnip',
-    'saadparwaiz1/cmp_luasnip',
+    {
+      "L3MON4D3/LuaSnip",
+      dependencies = {
+        "saadparwaiz1/cmp_luasnip",
+        'rafamadriz/friendly-snippets',
+      }
+    },
 
-    -- Adds LSP completion capabilities
+    -- Add LSP completion capabilities
     'hrsh7th/cmp-nvim-lsp',
-
-    -- Adds a number of user-friendly snippets
-    'rafamadriz/friendly-snippets',
   },
 }
