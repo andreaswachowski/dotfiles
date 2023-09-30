32793b022682b308e4a23e9399b639d48c2ad7ce
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-09-30 16:22:10 +0200

    refactor(nvim): move after/plugin/mason to lua/lsp/mason
    The after/plugin/mason.lua location was not too precise, because
    inside, also mason_lspconfig is required.
    
    Also, a dedicated "lsp"-directory sets the stage for better
    modularizing the settings for various languages.
    
    The idea is taken from LunarVim in
    https://github.com/LunarVim/Neovim-from-scratch/tree/55d29f82613aeeb755cbd3c62c46b8c24e2045d4/lua/user/lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,17 +5,18 @@ let &packpath = &runtimepath
 ]]
 
 local modules = {
-  "aw.globals",
-  "config.options",
-  "config.keymaps",
-  "autocommands",
-  "config.lazy",
+  'aw.globals',
+  'config.options',
+  'config.keymaps',
+  'autocommands',
+  'config.lazy',
+  'lsp',
 }
 
 for _, module in ipairs(modules) do
   local ok, err = pcall(require, module)
   if not ok then
-    error("Error loading " .. module .. "\n\n" .. err)
+    error('Error loading ' .. module .. '\n\n' .. err)
   end
 end
 
