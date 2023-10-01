c41da07cd22897d1232f8de23e41a47f0152dd7f
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-10-01 19:43:55 +0200

    style(nvim): format after/plugin/nvim-cmp.lua

--- .config/nvim/after/plugin/nvim-cmp.lua
+++ .config/nvim/after/plugin/nvim-cmp.lua
@@ -1,36 +1,36 @@
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
+local cmp = require('cmp')
+local luasnip = require('luasnip')
 require('luasnip.loaders.from_vscode').lazy_load()
-luasnip.config.setup {}
+luasnip.config.setup({})
 
 -- Disable distracting ins-completion-menu messages
-vim.opt.shortmess:append "c"
+vim.opt.shortmess:append('c')
 
-local ok, lspkind = pcall(require, "lspkind")
+local ok, lspkind = pcall(require, 'lspkind')
 if not ok then
   return
 end
 
 ---@diagnostic disable-next-line: missing-fields
-cmp.setup {
+cmp.setup({
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
     end,
   },
-  mapping = cmp.mapping.preset.insert {
+  mapping = cmp.mapping.preset.insert({
     ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete {},
-    ['<CR>'] = cmp.mapping.confirm {
+    ['<C-Space>'] = cmp.mapping.complete({}),
+    ['<CR>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
-    },
-  },
+    }),
+  }),
   -- The order of sources matter (by default). That gives them priority
   -- You can configure:
   --   keyword_length
@@ -42,19 +42,19 @@ cmp.setup {
     { name = 'nvim_lsp' },
     { name = 'path' },
     { name = 'luasnip' },
-    { name = 'buffer',  keyword_length = 5 },
+    { name = 'buffer', keyword_length = 5 },
   },
   ---@diagnostic disable-next-line: missing-fields
   formatting = {
-    format = lspkind.cmp_format {
+    format = lspkind.cmp_format({
       with_text = true,
       menu = {
-        buffer = "[buf]",
-        nvim_lsp = "[LSP]",
-        nvim_lua = "[api]",
-        path = "[path]",
-        lusanip = "[snip]"
-      }
-    }
-  }
-}
+        buffer = '[buf]',
+        nvim_lsp = '[LSP]',
+        nvim_lua = '[api]',
+        path = '[path]',
+        lusanip = '[snip]',
+      },
+    }),
+  },
+})
