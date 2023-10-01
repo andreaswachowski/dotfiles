e49fbb63d21e25b34cbcd5bba5d082c9e05c29e8
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-10-01 09:38:21 +0200

    refactor(nvim): extract LSP handlers into module
    This follows the approach from LunarVim, see
    https://github.com/LunarVim/Neovim-from-scratch/blob/55d29f82613aeeb755cbd3c62c46b8c24e2045d4/lua/user/lsp/init.lua

--- .config/nvim/lua/lsp/mason.lua
+++ .config/nvim/lua/lsp/mason.lua
@@ -1,74 +1,3 @@
--- [[ Configure LSP ]]
---  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is possible
-  -- to define small helper and utility functions so you don't have to repeat yourself
-  -- many times.
-  --
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
-
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
-
-  local function quickfix()
-    vim.lsp.buf.code_action({
-      filter = function(a)
-        return a.isPreferred
-      end,
-      apply = true,
-    })
-  end
-
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-  nmap('<leader>qf', quickfix, '[Q]uick[f]ix (apply code action)')
-
-  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap(
-    'gI',
-    require('telescope.builtin').lsp_implementations,
-    '[G]oto [I]mplementation'
-  )
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap(
-    '<leader>ds',
-    require('telescope.builtin').lsp_document_symbols,
-    '[D]ocument [S]ymbols'
-  )
-  nmap(
-    '<leader>ws',
-    require('telescope.builtin').lsp_dynamic_workspace_symbols,
-    '[W]orkspace [S]ymbols'
-  )
-
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  -- TODO: <C-k> conflicts with window navigation
-  -- nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap(
-    '<leader>wa',
-    vim.lsp.buf.add_workspace_folder,
-    '[W]orkspace [A]dd Folder'
-  )
-  nmap(
-    '<leader>wr',
-    vim.lsp.buf.remove_workspace_folder,
-    '[W]orkspace [R]emove Folder'
-  )
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
-end
-
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed.
 --
@@ -106,10 +35,6 @@ local servers = {
 -- Setup neovim lua configuration
 require('neodev').setup()
 
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
 -- Ensure the servers above are installed
 local mason_lspconfig = require('mason-lspconfig')
 
@@ -119,8 +44,8 @@ mason_lspconfig.setup({
 
 local default_handler = function(server)
   require('lspconfig')[server].setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
+    capabilities = require('lsp.handlers').capabilities,
+    on_attach = require('lsp.handlers').on_attach,
     filetypes = (servers[server] or {}).filetypes,
   })
 end
@@ -132,8 +57,8 @@ local lsp_handlers = {
 local build_lsp_configs = function()
   for _, server in pairs(servers) do
     local opts = {
-      on_attach = on_attach,
-      capabilities = capabilities,
+      on_attach = require('lsp.handlers').on_attach,
+      capabilities = require('lsp.handlers').capabilities,
     }
 
     server = vim.split(server, '@')[1]
@@ -210,6 +135,6 @@ require('lspconfig').ruby_ls.setup({
   -- so combine them into one function
   on_attach = function(client, bufnr)
     setup_diagnostics(client, bufnr)
-    on_attach(client, bufnr)
+    require('lsp.handlers').on_attach(client, bufnr)
   end,
 })
