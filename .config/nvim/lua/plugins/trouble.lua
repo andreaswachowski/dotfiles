37a6ae2a91af5eb98bcc59930d7c9ae8118eb050
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-10-01 09:35:23 +0200

    feat(nvim): add plugin folke/trouble.nvim

---
+++ .config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,10 @@
+return {
+  'folke/trouble.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    -- https://github.com/folke/trouble.nvim#%EF%B8%8F-configuration
+    mode = 'document_diagnostics',
+    auto_open = true,
+    auto_close = false,
+  },
+}
