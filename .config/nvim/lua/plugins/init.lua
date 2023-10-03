5d9c727b6615e5c87fa930d1cfe45dc56d4e4027
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-10-03 16:45:28 +0200

    refactor(nvim): extract nvim-treesitter setup to separate file

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -28,15 +28,6 @@ return {
   },
 
   {
-    -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    build = ':TSUpdate',
-  },
-
-  {
     'iamcco/markdown-preview.nvim',
     ft = 'markdown',
     build = function()
