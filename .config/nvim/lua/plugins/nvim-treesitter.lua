18de5621d454b51eb08a9084a0a708077f85fb61
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-10-03 17:08:46 +0200

    feat(nvim): enable a few treesitter parsers

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,4 +5,34 @@ return {
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
   build = ':TSUpdate',
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      auto_install = true,
+      ensure_installed = {
+        -- defaults
+        'c',
+        'lua',
+        'vim',
+        'vimdoc',
+        'query',
+
+        -- in addition:
+        'bash',
+        'css',
+        'cpp',
+        'dockerfile',
+        'gitignore',
+        'html',
+        'javascript',
+        'json',
+        'markdown',
+        'markdown_inline',
+        'python',
+        'ruby',
+        'tsx',
+        'typescript',
+        'yaml',
+      },
+    })
+  end,
 }
