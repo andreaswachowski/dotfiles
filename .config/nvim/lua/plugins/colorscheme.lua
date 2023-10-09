bcefceb2b84c1c94fabfd41caae7dda49255d827
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-10-09 17:57:03 +0200

    fix(nvim): use background as dark as terminal

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,11 +6,13 @@ return {
     -- Lighten some grays. Start with dark4 (== #7c6f64 == rgb(124 111 100))
     -- and add 50 each to R, G, and B, yielding rgb(174, 161, 150) = #95887d
     local dark5 = '#95887d'
-    local iterm2_bg = '#0e0e0e'
     local colors = require('gruvbox').palette
     require('gruvbox').setup({
       contrast = 'hard', -- background as dark as terminal
       invert_tabline = true,
+      palette_overrides = {
+        dark0_hard = '#0e0e0e', -- same as iTerm profile
+      },
       overrides = {
         SpecialKey = { fg = dark5 },
         Whitespace = { fg = dark5 }, -- shown with "set list" (":help listchars"
