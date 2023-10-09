6d6c6f013fde2b91e609844796262dbddada9e70
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-10-09 16:55:04 +0200

    feat(nvim): optimize lualine for small window width

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,50 @@
 local gitsigns_available, _ = pcall(require, 'gitsigns')
 
+-- https://github.com/nvim-lualine/lualine.nvim/wiki/Component-snippets#truncating-components-in-smaller-window
+--- @param trunc_width number trunctates component when screen width is less then trunc_width
+--- @param trunc_len number truncates component to trunc_len number of chars
+--- @param hide_width number hides component when window width is smaller then hide_width
+--- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
+--- return function that can format the component accordingly
+local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
+  return function(str)
+    local win_width = vim.fn.winwidth(0)
+    if hide_width and win_width < hide_width then
+      return ''
+    elseif
+      trunc_width
+      and trunc_len
+      and win_width < trunc_width
+      and #str > trunc_len
+    then
+      return str:sub(1, trunc_len) .. (no_ellipsis and '' or '...')
+    end
+    return str
+  end
+end
+
+local function trunc_branch(trunc_width, trunc_len, hide_width, no_ellipsis)
+  return function(str)
+    local win_width = vim.fn.winwidth(0)
+    if hide_width and win_width < hide_width then
+      return ''
+    elseif
+      trunc_width
+      and trunc_len
+      and win_width < trunc_width
+      and #str > trunc_len
+    then
+      if str:match('/') then
+        return string.sub(str:match('/.*'), 2, trunc_len)
+          .. (no_ellipsis and '' or '...')
+      else
+        return str:sub(1, trunc_len) .. (no_ellipsis and '' or '...')
+      end
+    end
+    return str
+  end
+end
+
 local function diff_source()
   local gitsigns = vim.b.gitsigns_status_dict
   if gitsigns then
@@ -29,15 +74,28 @@ return {
     sections = {
       lualine_a = {},
       lualine_b = {
-        'branch',
+        { 'branch', fmt = trunc_branch(120, 15, 80, false) },
         -- diff_source() and { 'diff', source = diff_source } or 'diff',
         gitsigns_available and { 'diff', source = diff_source }
           or 'diff',
         'diagnostics',
       },
-      lualine_c = { { 'filename', path = 1 } },
-      lualine_x = { 'encoding', 'fileformat', 'filetype' },
-      lualine_y = { 'progress' },
+      lualine_c = {
+        { 'filename', path = 1, fmt = trunc(90, 30, 50, false) },
+      },
+      lualine_x = {
+        {
+          'encoding',
+          fmt = function(str)
+            if str ~= 'utf-8' then
+              return str
+            end
+          end,
+        },
+        'fileformat',
+        'filetype',
+      },
+      lualine_y = { { 'progress', fmt = trunc(90, 3, 90, true) } },
       lualine_z = { 'location' },
     },
     inactive_sections = {
