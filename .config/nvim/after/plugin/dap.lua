6f37953bd86c4cb3c294e58c1ad7fe6ae3c4da28
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-10-14 19:59:54 +0200

    feat(nvim): select executable to debug with telescope
    Helpful with many small executables like for Leetcode.

--- .config/nvim/after/plugin/dap.lua
+++ .config/nvim/after/plugin/dap.lua
@@ -12,7 +12,33 @@ dap.configurations.cpp = {
     type = 'lldb',
     request = 'launch',
     program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      -- https://github.com/mfussenegger/nvim-dap/wiki/Cookbook#using-telescope-for-selecting-an-executable-to-debug
+      local pickers = require('telescope.pickers')
+      local finders = require('telescope.finders')
+      local conf = require('telescope.config').values
+      local actions = require('telescope.actions')
+      local action_state = require('telescope.actions.state')
+
+      return coroutine.create(function(coro)
+        local opts = {}
+        pickers
+          .new(opts, {
+            prompt_title = 'Path to executable',
+            finder = finders.new_oneshot_job(
+              { 'fd', '--hidden', '--no-ignore', '--type', 'x' },
+              {}
+            ),
+            sorter = conf.generic_sorter(opts),
+            attach_mappings = function(buffer_number)
+              actions.select_default:replace(function()
+                actions.close(buffer_number)
+                coroutine.resume(coro, action_state.get_selected_entry()[1])
+              end)
+              return true
+            end,
+          })
+          :find()
+      end)
     end,
     cwd = '${workspaceFolder}',
     stopOnEntry = false,
