7d03417d8d8774a473ad85cb64e7e3829597a607
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-10-14 23:53:48 +0200

    style(nvim): format after/plugin/luasnip.lua

--- .config/nvim/after/plugin/luasnip.lua
+++ .config/nvim/after/plugin/luasnip.lua
@@ -1,11 +1,9 @@
 local ls = require('luasnip')
 
-vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<C-H>", function() ls.jump(-1) end, { silent = true })
+vim.keymap.set({ 'i' }, '<C-K>', function() ls.expand() end, { silent = true })
+vim.keymap.set({ 'i', 's' }, '<C-L>', function() ls.jump(1) end, { silent = true })
+vim.keymap.set({ 'i', 's' }, '<C-H>', function() ls.jump(-1) end, { silent = true })
 
-vim.keymap.set({ "i", "s" }, "<C-E>", function()
-	if ls.choice_active() then
-		ls.change_choice(1)
-	end
+vim.keymap.set({ 'i', 's' }, '<C-E>', function()
+  if ls.choice_active() then ls.change_choice(1) end
 end, { silent = true })
