37dd1e6f5a3a62cdba1d9420a9a4a05b07dc520d
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-10-15 09:29:40 +0200

    feat(nvim): add shortcut to reload snippets
    Helpful when developing new snippets.
    
    https://youtu.be/Dn800rlPIho?si=pbqhvB_5NXyHsYhV&t=687

--- .config/nvim/after/plugin/luasnip.lua
+++ .config/nvim/after/plugin/luasnip.lua
@@ -9,4 +9,6 @@ vim.keymap.set({ 'i', 's' }, '<C-E>', function()
   if ls.choice_active() then ls.change_choice(1) end
 end, { silent = true })
 
+vim.keymap.set('n', '<leader><leader>s', '<cmd>source ~/.config/nvim/after/plugin/luasnip.lua<CR>')
+
 require('luasnip.loaders.from_vscode').lazy_load()
