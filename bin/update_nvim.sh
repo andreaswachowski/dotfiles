9c5de8c4f0dfff853d684b659442a512fa8476fb
Andreas Wachowski <andreas.wachowski@gmail.com>
2023-12-08 13:43:47 +0100

    fix(nvim): re-enable download in update_nvim.sh

--- bin/update_nvim.sh
+++ bin/update_nvim.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
+finish() {
+  rv=$?
+  cd - >/dev/null
+  exit $rv
+}
+
+trap finish EXIT INT TERM
+
 echo Before update:
 ls -l ~/local/nvim-macos/bin/nvim
 nvim --version
 
 cd ~/local || exit 1
 
-# curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz > nvim-macos.tar.gz
-# curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz.sha256sum > nvim-macos.tar.gz.sha256sum
+curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz >nvim-macos.tar.gz
+curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz.sha256sum >nvim-macos.tar.gz.sha256sum
 
-sha256sum --check < nvim-macos.tar.gz.sha256sum || exit 1
+sha256sum --check <nvim-macos.tar.gz.sha256sum || exit 1
 
 tar zxf nvim-macos.tar.gz
 
