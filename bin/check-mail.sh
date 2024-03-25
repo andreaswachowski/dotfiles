231f91dc27fd1d262029d82f0da3744ef2a92590
Andreas Wachowski <andreas.wachowski@gmail.com>
2024-03-25 11:47:36 +0100

    feat(check-mail): mbsync mailbox.org

--- bin/check-mail.sh
+++ bin/check-mail.sh
@@ -2,6 +2,7 @@
 #
 # https://gist.github.com/trev-dev/01a5fbf28127e0af08657bc6e332b32b
 
+mbsync mailbox
 mbsync gmail
 notmuch new
 notmuch tag --batch --input="${HOME}/.config/mutt/tagfile.notmuch"
