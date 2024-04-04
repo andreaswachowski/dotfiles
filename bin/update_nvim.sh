446b4a0baad68bfe42ac598fbfeddd5499b3741c
Andreas Wachowski <andreas.wachowski@gmail.com>
2024-04-04 08:38:40 +0200

    fix(bin): adjust update_nvim for Mac architecture

--- bin/update_nvim.sh
+++ bin/update_nvim.sh
@@ -6,22 +6,27 @@ finish() {
   exit $rv
 }
 
+ARCH=$(uname -m)
+
 trap finish EXIT INT TERM
 
 echo Before update:
-ls -l ~/local/nvim-macos/bin/nvim
+ls -l ~/local/"nvim-macos-$ARCH"/bin/nvim
 nvim --version
 
 cd ~/local || exit 1
 
-curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz >nvim-macos.tar.gz
-curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz.sha256sum >nvim-macos.tar.gz.sha256sum
+FILENAME=nvim-macos-$ARCH.tar.gz
+SHA256=$FILENAME.sha256sum
 
-sha256sum --check <nvim-macos.tar.gz.sha256sum || exit 1
+curl -L "https://github.com/neovim/neovim/releases/download/nightly/$FILENAME" >"$FILENAME"
+curl -L "https://github.com/neovim/neovim/releases/download/nightly/$SHA256" >"$SHA256"
 
-tar zxf nvim-macos.tar.gz
+sha256sum --check <"$SHA256" || exit 1
+
+tar zxf "$FILENAME"
 
 echo After update:
-ls -l ~/local/nvim-macos/bin/nvim
+ls -l ~/local/"nvim-macos-$ARCH"/bin/nvim
 nvim --version
 cd - || exit 1
