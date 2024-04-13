40b948e9ea5bfdc48a56ffa6ddfe2940fabed833
Andreas Wachowski <andreas.wachowski@gmail.com>
2024-04-13 14:53:36 +0200

    feat(check-mail): disable mbsync gmail

--- bin/check-mail.sh
+++ bin/check-mail.sh
@@ -3,6 +3,6 @@
 # https://gist.github.com/trev-dev/01a5fbf28127e0af08657bc6e332b32b
 
 mbsync mailbox
-mbsync gmail
+# mbsync gmail
 notmuch new
 notmuch tag --batch --input="${HOME}/.config/mutt/tagfile.notmuch"
