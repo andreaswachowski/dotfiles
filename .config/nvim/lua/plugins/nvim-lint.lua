andreaswachowski/dotfiles

commit 80c1c539c04be5f43addd87dcbbc379e8e07abbf

feat(nvim): add Ruby LSP
Ruby LSP provides its own diagnostics based on the bundled rubocop and
conveniently (contrary to Solargraph!) provides code actions to quickfix those.

Thus, disable nvim-lint's rubocop diagnostics when a bundle is present;
to avoid seeing duplicated diagnostics.

Given we have code actions, add a quickfix shortcut, found on
https://stackoverflow.com/questions/67988374/neovim-lsp-auto-fix-fix-current

(As another advantage over Solargraph, Ruby LSP apparently also does not need to
run "yard gems" for indexing, nor does it require a .solargraph.yml equivalent
or other contortions. It seems to work out of the box.)

.config/nvim/lua/plugins/nvim-lint.lua
@@ -1,21 +1,20 @@
 return {
   "mfussenegger/nvim-lint",
   config = function()
-    local rubocop = require('lint').linters.rubocop
     local fname = vim.api.nvim_buf_get_name(0)
-    if require("lspconfig").util.root_pattern("Gemfile")(fname) ~= nil then
-      rubocop.cmd = 'bundle'
-      rubocop.args = { 'exec', 'rubocop', '--format', 'json', '--force-exclusion', '--stdin', vim.api.nvim_buf_get_name(0) }
+    if require("lspconfig").util.root_pattern("Gemfile")(fname) == nil then
+      local rubocop = require('lint').linters.rubocop
+      rubocop.args = { '--format', 'json', '--force-exclusion', '--stdin', vim.api.nvim_buf_get_name(0) }
       rubocop.stdin = true
-    end
-    require('lint').linters_by_ft = {
-      ruby = {
-        "rubocop"
+      require('lint').linters_by_ft = {
+        ruby = {
+          "rubocop"
+        }
       }
-    }
-    vim.api.nvim_create_autocmd(
-      { "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" },
-      { callback = function() require("lint").try_lint() end, }
-    )
+      vim.api.nvim_create_autocmd(
+        { "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" },
+        { callback = function() require("lint").try_lint() end, }
+      )
+    end
   end
 }

.config/nvim/after/plugin/mason.lua
@@ -15,8 +15,16 @@ local on_attach = function(_, bufnr)
     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
   end
 
+  local function quickfix()
+    vim.lsp.buf.code_action({
+      filter = function(a) return a.isPreferred end,
+      apply = true
+    })
+  end
+
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  nmap('<leader>qf', quickfix, '[Q]uick[f]ix (apply code action)')
 
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
@@ -60,6 +68,7 @@ local servers = {
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
+  ruby_ls = {},
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
@@ -92,3 +101,59 @@ mason_lspconfig.setup_handlers {
     }
   end
 }
+
+-- [[ Ruby LSP Configuration }]
+-- https://github.com/Shopify/ruby-lsp/blob/main/EDITORS.md#neovim-lsp
+
+-- textDocument/diagnostic support until 0.10.0 is released
+---@diagnostic disable-next-line: lowercase-global
+_timers = {}
+local function setup_diagnostics(client, buffer)
+  if require("vim.lsp.diagnostic")._enable then
+    return
+  end
+
+  local diagnostic_handler = function()
+    local params = vim.lsp.util.make_text_document_params(buffer)
+    client.request("textDocument/diagnostic", { textDocument = params }, function(err, result)
+      if err then
+        local err_msg = string.format("diagnostics error - %s", vim.inspect(err))
+        vim.lsp.log.error(err_msg)
+      end
+      if not result then
+        return
+      end
+      vim.lsp.diagnostic.on_publish_diagnostics(
+        nil,
+        vim.tbl_extend("keep", params, { diagnostics = result.items }),
+        { client_id = client.id }
+      )
+    end)
+  end
+
+  diagnostic_handler() -- to request diagnostics on buffer when first attaching
+
+  vim.api.nvim_buf_attach(buffer, false, {
+    on_lines = function()
+      if _timers[buffer] then
+        vim.fn.timer_stop(_timers[buffer])
+      end
+      _timers[buffer] = vim.fn.timer_start(200, diagnostic_handler)
+    end,
+    on_detach = function()
+      if _timers[buffer] then
+        vim.fn.timer_stop(_timers[buffer])
+      end
+    end,
+  })
+end
+
+require('lspconfig').ruby_ls.setup({
+  -- We want both the diagnostics (for the code actions)
+  -- and the keyboard shortcuts from the general setup,
+  -- so combine them into one function
+  on_attach = function(client, bufnr)
+    setup_diagnostics(client, bufnr)
+    on_attach(client, bufnr)
+  end
+})

commit 4038f53e474e8bafbe679c8c77d11155dd560d83

fix(nvim): fix wrong end-placement in nvim-lint.lua
This was introduced with 80c1c53 - the "end" in

    if require("lspconfig").util.root_pattern("Gemfile") ...
      ...
    end

must happen before the require('lint').linters_by_ft directive.

.config/nvim/lua/plugins/nvim-lint.lua
@@ -12,28 +12,23 @@ return {
         vim.api.nvim_buf_get_name(0),
       }
       rubocop.stdin = true
-      require('lint').linters_by_ft = {
-        css = { 'stylelint' },
-        dockerfile = { 'hadolint' },
-        javascript = { 'eslint' },
-        javascriptreact = { 'eslint' },
-        json = { 'jsonlint' },
-        markdown = { 'vale' },
-        ruby = { 'rubocop' },
-        sh = { 'shellcheck' },
-        typescript = { 'eslint' },
-        typescriptreact = { 'eslint' },
-        yaml = { 'yamllint' },
-        ['yaml.ansible'] = { 'ansible_lint' },
-      }
-      vim.api.nvim_create_autocmd(
-        { 'BufEnter', 'BufWritePost', 'InsertLeave', 'TextChanged' },
-        {
-          callback = function()
-            require('lint').try_lint()
-          end,
-        }
-      )
     end
+    require('lint').linters_by_ft = {
+      css = { 'stylelint' },
+      dockerfile = { 'hadolint' },
+      javascript = { 'eslint' },
+      javascriptreact = { 'eslint' },
+      json = { 'jsonlint' },
+      markdown = { 'vale' },
+      ruby = { 'rubocop' },
+      sh = { 'shellcheck' },
+      typescript = { 'eslint' },
+      typescriptreact = { 'eslint' },
+      yaml = { 'yamllint' },
+      ['yaml.ansible'] = { 'ansible_lint' },
+    }
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave', 'TextChanged' }, {
+      callback = function() require('lint').try_lint() end,
+    })
   end,
 }