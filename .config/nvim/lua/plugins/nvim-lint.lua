491daaf29e46c806a3706a67914d07e11225d5ce
Andreas Wachowski <andreas.wachowski@gmail.com>
2024-04-20 09:53:12 +0200

    docs(nvim): add hint in nvim-lint

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -28,7 +28,15 @@ return {
       ['yaml.ansible'] = { 'ansible_lint' },
     }
     vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave', 'TextChanged' }, {
-      callback = function() require('lint').try_lint() end,
+      callback = function()
+        require('lint').try_lint()
+        -- The linting works even with monorepos, but just in case
+        -- it wouldn't, there's an idea here from
+        -- https://github.com/mfussenegger/nvim-lint/issues/482
+        -- local get_clients = vim.lsp.get_clients
+        -- local client = get_clients({ bufnr = 0 })[1] or {}
+        -- require('lint').try_lint(nil, { cwd = client.root_dir })
+      end,
     })
   end,
 }
