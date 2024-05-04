139c585618d227ef38cd1fd08271e3d6067ff68f
Andreas Wachowski <andreas.wachowski@gmail.com>
2024-05-04 11:11:06 +0200

    refactor(nvim): move treesitter config into opts
    Inspired from https://github.com/nvim-lua/kickstart.nvim/pull/732

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -7,90 +7,91 @@ return {
     'RRethy/nvim-treesitter-endwise',
   },
   build = ':TSUpdate',
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require('nvim-treesitter.configs').setup({
-      autotag = {
-        enable = true,
-      },
-      endwise = {
-        enable = true,
-      },
-      auto_install = true,
-      ensure_installed = {
-        -- defaults
-        'c',
-        'lua',
-        'vim',
-        'vimdoc',
-        'query',
+  opts = {
+    autotag = {
+      enable = true,
+    },
+    endwise = {
+      enable = true,
+    },
+    auto_install = true,
+    ensure_installed = {
+      -- defaults
+      'c',
+      'lua',
+      'vim',
+      'vimdoc',
+      'query',
 
-        -- in addition:
-        'bash',
-        'css',
-        'cpp',
-        'dockerfile',
-        'gitignore',
-        'html',
-        'javascript',
-        'json',
-        'markdown',
-        'markdown_inline',
-        'python',
-        'ruby',
-        'tsx',
-        'typescript',
-        'yaml',
-      },
-      matchup = {
-        -- https://github.com/andymass/vim-matchup/issues/198
+      -- in addition:
+      'bash',
+      'css',
+      'cpp',
+      'dockerfile',
+      'gitignore',
+      'html',
+      'javascript',
+      'json',
+      'markdown',
+      'markdown_inline',
+      'python',
+      'ruby',
+      'tsx',
+      'typescript',
+      'yaml',
+    },
+    matchup = {
+      -- https://github.com/andymass/vim-matchup/issues/198
+      enable = true,
+      disable = { 'ruby' },
+    },
+    textobjects = {
+      select = {
         enable = true,
-        disable = { 'ruby' },
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ['aa'] = '@parameter.outer',
-            ['ia'] = '@parameter.inner',
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            [']m'] = '@function.outer',
-            [']]'] = '@class.outer',
-          },
-          goto_next_end = {
-            [']M'] = '@function.outer',
-            [']['] = '@class.outer',
-          },
-          goto_previous_start = {
-            ['[m'] = '@function.outer',
-            ['[['] = '@class.outer',
-          },
-          goto_previous_end = {
-            ['[M'] = '@function.outer',
-            ['[]'] = '@class.outer',
-          },
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ['<leader>a'] = '@parameter.inner',
-          },
-          swap_previous = {
-            ['<leader>A'] = '@parameter.inner',
-          },
+        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ['aa'] = '@parameter.outer',
+          ['ia'] = '@parameter.inner',
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
         },
       },
-    })
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          [']m'] = '@function.outer',
+          [']]'] = '@class.outer',
+        },
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']['] = '@class.outer',
+        },
+        goto_previous_start = {
+          ['[m'] = '@function.outer',
+          ['[['] = '@class.outer',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[]'] = '@class.outer',
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ['<leader>a'] = '@parameter.inner',
+        },
+        swap_previous = {
+          ['<leader>A'] = '@parameter.inner',
+        },
+      },
+    },
+  },
+  config = function(_, opts)
+    ---@diagnostic disable-next-line: missing-fields
+    require('nvim-treesitter.configs').setup(opts)
   end,
 }
