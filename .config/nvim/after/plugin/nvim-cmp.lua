3ec50f357c756b3b800c200b4bc9af073fa52d41
Andreas Wachowski <mail@andreas-wachowski.de>
2024-05-09 08:41:29 +0200

    feat(nvim): add plugin nvim-orgmode/orgmode

--- .config/nvim/after/plugin/nvim-cmp.lua
+++ .config/nvim/after/plugin/nvim-cmp.lua
@@ -39,6 +39,7 @@ cmp.setup({
   sources = {
     { name = 'nvim_lua' },
     { name = 'nvim_lsp' },
+    { name = 'orgmode' },
     { name = 'path' },
     { name = 'luasnip' },
     { name = 'buffer', keyword_length = 5 },
---
+++ .config/nvim/lua/plugins/orgmode.lua
@@ -0,0 +1,30 @@
+local org_path = function(path)
+  local org_directory = '~/Documents/git_projects/notes'
+  return ('%s/%s'):format(org_directory, path)
+end
+
+return {
+  'nvim-orgmode/orgmode',
+  event = 'VeryLazy',
+  ft = { 'org', 'orgagenda' },
+  config = function()
+    -- Setup orgmode
+    require('orgmode').setup({
+      org_agenda_files = { org_path('**/*.org'), org_path('**/*.org_archive') },
+      org_default_notes_file = org_path('refile.org'),
+      org_capture_templates = {
+        t = {
+          description = 'Refile',
+          template = '* TODO %?\nCREATED: %U',
+        },
+      },
+    })
+
+    -- NOTE: If you are using nvim-treesitter with `ensure_installed = "all"` option
+    -- add `org` to ignore_install
+    -- require('nvim-treesitter.configs').setup({
+    --   ensure_installed = 'all',
+    --   ignore_install = { 'org' },
+    -- })
+  end,
+}
