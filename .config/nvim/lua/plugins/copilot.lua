ed85066a1f11f213a1202005b2a06839370d65ca
Andreas Wachowski <mail@andreas-wachowski.de>
2024-05-11 21:06:42 +0200

    feat(nvim): add plugins zbirenbaum/copilot.lua and CopilotC-Nvim/CopilotChat.nvim

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,6 @@
+return {
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  -- config in after/plugin/copilot.lua
+}
---
+++ .config/nvim/lua/plugins/copilotchat.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'canary',
+    dependencies = {
+      { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+    -- Configuration see
+    -- https://github.com/jellydn/lazy-nvim-ide/blob/main/lua/plugins/extras/copilot-chat-v2.lua
+  },
+}
