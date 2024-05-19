4ec566c4003af60546ddada33e04620f4126ef0b
Andreas Wachowski <mail@andreas-wachowski.de>
2024-05-19 18:55:09 +0200

    fix(nvim): disable rubocop autocommand

--- .config/nvim/lua/autocommands/ruby.lua
+++ .config/nvim/lua/autocommands/ruby.lua
@@ -1,13 +1,15 @@
 -- https://docs.rubocop.org/rubocop/1.63/usage/lsp.html#neovim-nvim-lspconfig
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'ruby',
-  callback = function()
-    vim.lsp.start({
-      name = 'rubocop',
-      cmd = { 'bundle', 'exec', 'rubocop', '--lsp' },
-    })
-  end,
-})
+-- This is apparently redundant because it already works with the lspconfig setup
+-- in lua/lsp/mason.lua
+-- vim.api.nvim_create_autocmd('FileType', {
+--   pattern = 'ruby',
+--   callback = function()
+--     vim.lsp.start({
+--       name = 'rubocop',
+--       cmd = { 'bundle', 'exec', 'rubocop', '--lsp' },
+--     })
+--   end,
+-- })
 vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*.rb',
   callback = function() vim.lsp.buf.format() end,
