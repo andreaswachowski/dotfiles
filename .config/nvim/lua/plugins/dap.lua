5d01e5582f1f931956e6ee2464db0d51082c34b6
Andreas Wachowski <mail@andreas-wachowski.de>
2024-05-26 16:42:14 +0200

    refactor(nvim): declare nvim-dap dependencies
    As seen in
    https://github.com/tjdevries/config.nvim/blob/master/lua/custom/plugins/dap.lua

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,7 +1,9 @@
 return {
   'mfussenegger/nvim-dap',
-  'rcarriga/nvim-dap-ui',
-  'theHamsta/nvim-dap-virtual-text',
-  'nvim-telescope/telescope-dap.nvim',
+  dependencies = {
+    'rcarriga/nvim-dap-ui',
+    'theHamsta/nvim-dap-virtual-text',
+    'nvim-telescope/telescope-dap.nvim',
+  },
   -- config is in after/plugin/dap.lua !
 }
