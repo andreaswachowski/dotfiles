73d4bca59b0a6637d2864cf778c652dad7a514fc
Andreas Wachowski <andreas.wachowski@gmail.com>
2024-06-22 23:12:57 +0200

    feat(nvim): unmap <C-K> in luasnip

--- .config/nvim/after/plugin/luasnip.lua
+++ .config/nvim/after/plugin/luasnip.lua
@@ -1,7 +1,9 @@
 local ls = require('luasnip')
 ls.config.set_config({})
 
-vim.keymap.set({ 'i' }, '<C-K>', function() ls.expand() end, { silent = true })
+-- https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+-- uses <C-K> instead of <C-J>, but <C-K> is needed for digraphs
+vim.keymap.set({ 'i' }, '<C-J>', function() ls.expand() end, { silent = true })
 vim.keymap.set({ 'i', 's' }, '<C-L>', function() ls.jump(1) end, { silent = true })
 vim.keymap.set({ 'i', 's' }, '<C-H>', function() ls.jump(-1) end, { silent = true })
 
