879779e965ddcc7b93a8b017bdcb28ce98c67694
Andreas Wachowski <mail@andreas-wachowski.de>
2024-06-30 08:54:09 +0200

    chore(nvim): update gitsigns.nvim (4a143f1 -> 6b1a14e)
    yadm support was removed but fortunately, yadm-gitsigns.nvim
    is available.
    
    https://github.com/lewis6991/gitsigns.nvim/pull/1055

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,15 @@
 return {
   -- Adds git related signs to the gutter, as well as utilities for managing changes
   'lewis6991/gitsigns.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    {
+      'seanbreckenridge/gitsigns-yadm.nvim',
+      opts = {
+        shell_timeout_ms = 1000,
+      },
+    },
+  },
   opts = {
     -- See `:help gitsigns.txt`
     signs = {
@@ -10,6 +19,7 @@ return {
       topdelete = { text = '‾' },
       changedelete = { text = '~' },
     },
+    _on_attach_pre = function(_, callback) require('gitsigns-yadm').yadm_signs(callback) end,
     on_attach = function(bufnr)
       vim.keymap.set(
         'n',
@@ -21,21 +31,13 @@ return {
       -- don't override the built-in and fugitive keymaps
       local gs = package.loaded.gitsigns
       vim.keymap.set({ 'n', 'v' }, ']c', function()
-        if vim.wo.diff then
-          return ']c'
-        end
-        vim.schedule(function()
-          gs.next_hunk()
-        end)
+        if vim.wo.diff then return ']c' end
+        vim.schedule(function() gs.next_hunk() end)
         return '<Ignore>'
       end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
       vim.keymap.set({ 'n', 'v' }, '[c', function()
-        if vim.wo.diff then
-          return '[c'
-        end
-        vim.schedule(function()
-          gs.prev_hunk()
-        end)
+        if vim.wo.diff then return '[c' end
+        vim.schedule(function() gs.prev_hunk() end)
         return '<Ignore>'
       end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
     end,
