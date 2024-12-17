51f0c66913a79745e3a989285d56695fa737efc9
Andreas Wachowski <mail@andreas-wachowski.de>
2024-12-17 10:46:30 +0100

    fix(cleanup): remove files first, then directories
    From find's man page:
    
        The -delete primary will fail to delete a directory if it is not empty.

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 # vi: tw=0
 
-
 VIMSWAPDIR=$HOME/tmp/vim
 REGEX="$(echo $(pgrep vim) "$VIMSWAPDIR" | tr ' ' '|')"
 
 case $(uname) in
-    Linux)
-        FIND_OPTS="$VIMSWAPDIR -regextype posix-extended"
-        ;;
-    Darwin)
-        FIND_OPTS="-E $VIMSWAPDIR"
-        ;;
-    *)
-        echo Unknown platform, aborting
-        exit 1
-        ;;
+Linux)
+  FIND_OPTS="$VIMSWAPDIR -regextype posix-extended"
+  ;;
+Darwin)
+  FIND_OPTS="-E $VIMSWAPDIR"
+  ;;
+*)
+  echo Unknown platform, aborting
+  exit 1
+  ;;
 esac
 
 # Cleanup vim swap files
 # see http://stackoverflow.com/questions/762348/how-can-i-exclude-all-permission-denied-messages-from-find
 # for a discussion on the error output redirection
 
-
-find $FIND_OPTS -type d ! -regex ".*($REGEX)" -delete 2> >(grep -v 'relative path potentially not safe' >&2)
+find $FIND_OPTS -type f -user andreas ! -regex ".*($REGEX)" -delete 2> >(grep -v 'Permission denied' >&2)
+find $FIND_OPTS -type d -user andreas ! -regex ".*($REGEX)" -delete 2> >(grep -v 'Permission denied' >&2)
