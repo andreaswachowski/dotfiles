59a0344226f13ea497d827fe865daf32224fb618
Andreas Wachowski <mail@andreas-wachowski.de>
2024-12-17 11:31:33 +0100

    feat(cleanup): add dry-run mode

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -6,6 +6,26 @@ FIND=$(command -v find)
 VIMSWAPDIR=$HOME/tmp/vim
 REGEX="$(echo $(pgrep vim) "$VIMSWAPDIR" | tr ' ' '|')"
 
+DRY_RUN=false
+
+while getopts "n" opt; do
+  case $opt in
+  n)
+    DRY_RUN=true
+    ;;
+  *)
+    echo "Usage: $0 [-n] (dry-run mode)"
+    exit 1
+    ;;
+  esac
+done
+
+if [ "$DRY_RUN" = true ]; then
+  ACTION="-print"
+else
+  ACTION="-delete"
+fi
+
 case $(uname) in
 Linux)
   FIND_OPTS="$VIMSWAPDIR -regextype posix-extended"
@@ -23,11 +43,11 @@ esac
 # see http://stackoverflow.com/questions/762348/how-can-i-exclude-all-permission-denied-messages-from-find
 # for a discussion on the error output redirection
 
-$FIND $FIND_OPTS -type f -user andreas ! -regex ".*($REGEX)" -delete 2> >(grep -v 'Permission denied' >&2)
-$FIND $FIND_OPTS -type d -user andreas ! -regex ".*($REGEX)" -delete 2> >(grep -v 'Permission denied' >&2)
+$FIND $FIND_OPTS -type f -user andreas ! -regex ".*($REGEX)" $ACTION 2> >(grep -v 'Permission denied' >&2)
+$FIND $FIND_OPTS -type d -user andreas ! -regex ".*($REGEX)" $ACTION 2> >(grep -v 'Permission denied' >&2)
 
 $FIND $HOME/{Downloads,tmp} -mindepth 1 \
-  -mount -mtime +60 ! -name '$HOME/tmp/vim/*' -delete 2> >(grep -v 'Permission denied' >&2)
+  -mount -mtime +60 ! -name '$HOME/tmp/vim/*' $ACTION 2> >(grep -v 'Permission denied' >&2)
 $FIND $HOME/Desktop -mindepth 1 -type f \
   \( -name 'Screen Shot *' -o -name 'Screenshot *' -o -name 'Screen Rec*' \) \
-  -mount -mtime +60 -delete
+  -mount -mtime +60 $ACTION
