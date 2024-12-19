3b5f9e7391fd3b9294a05232013281c94197b28f
Andreas Wachowski <mail@andreas-wachowski.de>
2024-12-19 09:18:44 +0100

    feat(check-mail): move archived mails to year folder

--- bin/check-mail.sh
+++ bin/check-mail.sh
@@ -3,6 +3,21 @@
 # https://gist.github.com/trev-dev/01a5fbf28127e0af08657bc6e332b32b
 
 mbsync mailbox
+
+ARCHIVE_PATH="$HOME/Mail/mailbox.org/Archive"
+for email in "$ARCHIVE_PATH/"{new,cur}/*; do
+  if [[ -r "$email" ]]; then
+    # Extract the year from the email's Date header
+    year=$(grep -m1 '^Date:' "$email" | awk '{print $5}')
+    if [[ -n "$year" ]] && [[ "$year" =~ ^20.* ]]; then
+      mkdir -p "$ARCHIVE_PATH.$year/"{cur,new,tmp}
+      mv "$email" "$ARCHIVE_PATH.$year/cur"
+    else
+      echo Cannot move $email
+    fi
+  fi
+done
+
 # mbsync gmail
 notmuch new
 notmuch tag --batch --input="${HOME}/.config/mutt/tagfile.notmuch"
