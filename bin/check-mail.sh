906a98458d2fd2a5db189e6ebe6c339cb3829725
Andreas Wachowski <mail@andreas-wachowski.de>
2024-12-29 19:08:47 +0100

    fix(checkmail): use mblaze to move files
    The normal move does not remove the messages UID, leading to conflicts
    on the next mbsync.

--- bin/check-mail.sh
+++ bin/check-mail.sh
@@ -2,6 +2,21 @@
 #
 # https://gist.github.com/trev-dev/01a5fbf28127e0af08657bc6e332b32b
 
+set -e
+
+check_available() {
+  if ! command -v "$1" &>/dev/null; then
+    echo "Error: $1 is not available. ${2:-}" >&2
+    exit 1
+  fi
+}
+
+# Check for required commands
+check_available mbsync "brew install isync"
+check_available mmkdir "Download and make/make install https://github.com/leahneukirchen/mblaze"
+check_available mrefile "Download and make/make install https://github.com/leahneukirchen/mblaze"
+check_available notmuch "brew install notmuch"
+
 mbsync mailbox
 
 ARCHIVE_PATH="$HOME/Mail/mailbox.org/Archive"
@@ -10,10 +25,10 @@ for email in "$ARCHIVE_PATH/"{new,cur}/*; do
     # Extract the year from the email's Date header
     year=$(grep -m1 '^Date:' "$email" | awk '{print $5}')
     if [[ -n "$year" ]] && [[ "$year" =~ ^20.* ]]; then
-      mkdir -p "$ARCHIVE_PATH.$year/"{cur,new,tmp}
-      mv "$email" "$ARCHIVE_PATH.$year/cur"
+      mmkdir "$ARCHIVE_PATH.$year"
+      mrefile -v "$email" "$ARCHIVE_PATH.$year/cur"
     else
-      echo Cannot move $email
+      echo Cannot move "$email"
     fi
   fi
 done
