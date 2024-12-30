937917e183fbe212b5a53345e4c371b2ffc10333
Andreas Wachowski <mail@andreas-wachowski.de>
2024-12-30 11:49:34 +0100

    feat(mk_yearly_dirs): update for 2025

--- bin/mk_yearly_dirs.sh
+++ bin/mk_yearly_dirs.sh
@@ -12,23 +12,21 @@ YEAR=$1
 
 mkdir "$HOME/Documents/ownCloud/$YEAR"
 cd "$HOME/Documents/ownCloud/$YEAR"
-for month in 01_january 02_february 03_march 04_april 05_may 06_june 07_july 08_august 09_september 10_october 11_november 12_december
-do
+for month in 01_january 02_february 03_march 04_april 05_may 06_june 07_july 08_august 09_september 10_october 11_november 12_december; do
   mkdir $month
 done
 mkdir -p doc/magazines/{acm_computing_surveys,cacm,the_atlantic}
-mkdir -p finanzen/{heizung,dkb,strom,steuererklärung,smartbroker,barclays,finanzmanager,anlage,rechnungen}
+mkdir -p finanzen/{heizung,dkb,strom,steuererklärung,flatex,barclays,finanzmanager,anlage,rechnungen}
 mkdir -p finanzen/rechnungen/telekom/{festnetz,mobilfunk}
 mkdir -p fortbildung/{lectures,books,exercises,anki,youtube}
-mkdir -p wohnung
 mkdir -p briefe_und_karten
 
-NOTES=~/Documents/ownCloud/notes/$YEAR
-mkdir "$NOTES"
-cd "$NOTES"
-mkdir -p reading/articles
-mkdir -p reading/books
-mkdir todos
+# NOTES=~/Documents/ownCloud/notes/$YEAR
+# mkdir "$NOTES"
+# cd "$NOTES"
+# mkdir -p reading/articles
+# mkdir -p reading/books
+# mkdir todos
 
 cd "$CURRENT_DIR"
 echo "Don't forget to archive old directories"
