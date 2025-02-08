98ef4bd11de5cbd8155a3b9f0f206077573525ff
Andreas Wachowski <mail@andreas-wachowski.de>
2025-02-08 09:54:42 +0100

    fix(hammerspoon): fix incorrect symbolic links in setup.sh

--- .config/hammerspoon/setup.sh
+++ .config/hammerspoon/setup.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-if [ ! -d ~/.hammerspoon ]; then
-  mkdir -f ~/.hammerspoon
+SCRIPT_DIR="$(cd -- "$(dirname -- "$0")" && pwd)"
 
-  for file in init.lua .luarc.json; do
-    if [ ! -f ~/.hammerspoon/$file ]; then
-      ln -s $file ~/.hammerspoon/$file
-    fi
-  done
+if [ ! -d ~/.hammerspoon ]; then
+  mkdir ~/.hammerspoon
 fi
+
+for file in init.lua .luarc.json; do
+  if [ ! -f ~/.hammerspoon/$file ]; then
+    ln -s "$SCRIPT_DIR/$file" ~/.hammerspoon/$file
+  fi
+done
