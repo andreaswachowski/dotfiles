35da23d0fbac9fb617f74c05465b0d80391ca9f0
Andreas Wachowski <mail@andreas-wachowski.de>
2025-05-01 20:26:03 +0200

    feat(nvim): use cpplint to lint cpp

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -2,6 +2,7 @@ return {
   'mfussenegger/nvim-lint',
   config = function()
     require('lint').linters_by_ft = {
+      cpp = { 'cpplint' },
       css = { 'stylelint' },
       dockerfile = { 'hadolint' },
       gitcommit = { 'vale' },
