34a616f73854a4e60c58f82d1e095a7dceee1542
Andreas Wachowski <mail@andreas-wachowski.de>
2025-05-18 20:06:21 +0200

    feat(nvim): tweak formatoptions for text and mail files

--- .config/nvim/lua/autocommands/init.lua
+++ .config/nvim/lua/autocommands/init.lua
@@ -4,3 +4,15 @@ require('autocommands.ruby')
 require('autocommands.telescope')
 require('autocommands.ruby_stacktrace')
 vim.cmd([[autocmd FileType json,html,xml,yaml set tabstop=2 shiftwidth=2 expandtab]])
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'text,mail',
+  callback = function()
+    vim.opt_local.formatoptions:append('n') -- [fo-n] recognize lists (formatlistpat)
+    vim.opt_local.formatoptions:remove('q') -- [fo-n] recognize lists (formatlistpat)
+    vim.opt_local.autoindent = true
+  end,
+})
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'text,mail',
+  callback = function() vim.opt_local.formatlistpat = [[^\s*\(\d\+[):.}]\|[*-]\)\s*]] end,
+})
