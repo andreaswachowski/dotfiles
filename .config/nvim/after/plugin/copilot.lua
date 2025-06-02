6361d9fa8e4a32d7cfd25f72b0be52ea7b5761b9
Andreas Wachowski <mail@andreas-wachowski.de>
2025-06-02 14:16:10 +0200

    feat(nvim): execute after/plugin/copilot only when plugin available
    I don't have the plugin enabled on all machines.

--- .config/nvim/after/plugin/copilot.lua
+++ .config/nvim/after/plugin/copilot.lua
@@ -1,44 +1,47 @@
-require('copilot').setup({
-  panel = {
-    enabled = true,
-    auto_refresh = false,
-    keymap = {
-      jump_prev = '[[',
-      jump_next = ']]',
-      accept = '<CR>',
-      refresh = 'gr',
-      open = '<M-CR>',
+local ok, copilot = pcall(require, 'copilot')
+if ok then
+  copilot.setup({
+    panel = {
+      enabled = true,
+      auto_refresh = false,
+      keymap = {
+        jump_prev = '[[',
+        jump_next = ']]',
+        accept = '<CR>',
+        refresh = 'gr',
+        open = '<M-CR>',
+      },
+      layout = {
+        position = 'bottom', -- | top | left | right
+        ratio = 0.4,
+      },
     },
-    layout = {
-      position = 'bottom', -- | top | left | right
-      ratio = 0.4,
+    suggestion = {
+      enabled = true,
+      auto_trigger = false,
+      debounce = 75,
+      keymap = {
+        accept = '<M-l>',
+        accept_word = false,
+        accept_line = false,
+        next = '<M-]>',
+        prev = '<M-[>',
+        dismiss = '<C-]>',
+      },
     },
-  },
-  suggestion = {
-    enabled = true,
-    auto_trigger = false,
-    debounce = 75,
-    keymap = {
-      accept = '<M-l>',
-      accept_word = false,
-      accept_line = false,
-      next = '<M-]>',
-      prev = '<M-[>',
-      dismiss = '<C-]>',
+    filetypes = {
+      yaml = false,
+      markdown = false,
+      help = false,
+      gitcommit = false,
+      gitrebase = false,
+      hgcommit = false,
+      svn = false,
+      cvs = false,
+      ['.'] = false,
+      lua = true,
     },
-  },
-  filetypes = {
-    yaml = false,
-    markdown = false,
-    help = false,
-    gitcommit = false,
-    gitrebase = false,
-    hgcommit = false,
-    svn = false,
-    cvs = false,
-    ['.'] = false,
-    lua = true,
-  },
-  copilot_node_command = 'node', -- Node.js version must be > 18.x
-  server_opts_overrides = {},
-})
+    copilot_node_command = 'node', -- Node.js version must be > 18.x
+    server_opts_overrides = {},
+  })
+end
