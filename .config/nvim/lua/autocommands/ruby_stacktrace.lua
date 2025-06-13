8b2abbcbc014b29533d49827bf1177ea29587fd4
Andreas Wachowski <541365+andreaswachowski@users.noreply.github.com>
2025-06-13 15:52:12 +0200

    feat(nvim): widen regex to detect Ruby stacktrace (for RubyStacktrace syntax)

--- .config/nvim/lua/autocommands/ruby_stacktrace.lua
+++ .config/nvim/lua/autocommands/ruby_stacktrace.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd('BufRead', {
   group = 'RubyStacktrace',
   -- pattern = 'quickfix',
   callback = function()
-    if vim.fn.getline('1'):match(os.getenv('HOME') .. '.*ruby/gems') then
+    if vim.fn.getline('1'):match(os.getenv('HOME') .. '.*%.rb:%d+:.*') then
       vim.opt_local.syntax = 'ruby_stacktrace'
       vim.cmd('setlocal nowrap')
     end
