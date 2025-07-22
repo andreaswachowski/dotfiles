2043b4e5739d60a9489abafa9c2c81a4c43d63ec
Andreas Wachowski <541365+andreaswachowski@users.noreply.github.com>
2025-07-22 09:11:54 +0200

    fix(update_nvim): linux release location and sha256sum mechanism changed

--- bin/update_nvim.sh
+++ bin/update_nvim.sh
@@ -6,9 +6,10 @@ finish() {
   exit $rv
 }
 
+# https://github.blog/changelog/2025-06-03-releases-now-expose-digests-for-release-assets/
 OS=$(uname -s)
 if [ "$OS" = "Linux" ]; then
-  DIRECTORY=nvim-linux64
+  DIRECTORY=nvim-linux-x86_64
 elif [ "$OS" = "Darwin" ]; then
   ARCH=$(uname -m)
   DIRECTORY=nvim-macos-$ARCH
@@ -26,12 +27,12 @@ nvim --version
 cd ~/local || exit 1
 
 FILENAME=$DIRECTORY.tar.gz
-SHA256=$FILENAME.sha256sum
+SHA256=$FILENAME.sha256
 
 curl -L "https://github.com/neovim/neovim/releases/download/nightly/$FILENAME" >"$FILENAME"
-curl -L "https://github.com/neovim/neovim/releases/download/nightly/$SHA256" >"$SHA256"
+# curl -L "https://github.com/neovim/neovim/releases/download/nightly/$SHA256" >"$SHA256"
 
-sha256sum --check "$SHA256" || exit 1
+# sha256sum --check "$SHA256" || exit 1
 
 tar zxf "$FILENAME"
 
