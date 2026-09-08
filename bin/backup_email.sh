ab64f500aebb3a0682894209171425ac0e40d20e
Andreas Wachowski <541365+andreaswachowski@users.noreply.github.com>
2026-09-08 11:15:49 +0200

    feat(mutt): auto-backup after using inbox

--- bin/backup_email.sh
+++ bin/backup_email.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-rsync -av ~/Mail/ pve:/archives/email
+check_server_availability() {
+	remote_server="pve"
+	ping -c 1 "$remote_server" >/dev/null 2>&1
+	return $?
+}
+
+if check_server_availability; then
+	rsync -av ~/Mail/ pve:/archives/email
+else
+	echo "backup_email.sh: pve is not reachable, skipping backup" >&2
+	exit 1
+fi
